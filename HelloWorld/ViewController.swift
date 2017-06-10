93b8c4d2f90b093c175a848dbb8387354fd2490c
Jakob Daugherty <jadppf@mail.missouri.edu>
2017-06-09 20:27:35 -0500

    Final Answer

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -9,17 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var tempText: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    @IBAction func helloAction(_ sender: UIButton) {
+        tempText.text = "Hello World!"
+    }
 
+    @IBAction func clearAction(_ sender: UIButton) {
+        tempText.text = ""
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    
 
 }
 
