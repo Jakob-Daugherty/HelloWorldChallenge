ed069c56323212c32262f450201e42e0be60a1d9
Jakob Daugherty <jadppf@mail.missouri.edu>
2017-06-19 15:15:50 -0500

    First commit of project

--- Hello World/Hello World/ViewController.swift
+++ Hello World/Hello World/ViewController.swift
@@ -1,32 +1,25 @@
 //
 //  ViewController.swift
-//  HelloWorld
+//  Hello World
 //
-//  Created by Neil Smyth on 10/4/16.
-//  Copyright © 2016 eBookFrenzy. All rights reserved.
+//  Created by Jakob Daugherty on 6/19/17.
+//  Copyright © 2017 JakobDaugherty. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var tempText: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-    @IBAction func helloAction(_ sender: UIButton) {
-        tempText.text = "Hello World!"
-    }
 
-    @IBAction func clearAction(_ sender: UIButton) {
-        tempText.text = ""
-    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-    
 
 }
 
